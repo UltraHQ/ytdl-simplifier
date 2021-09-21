57dd7150e2081bcd20ae23e00afabe889825c28e
UltraHQ <35925402+UltraHQ@users.noreply.github.com>
2021-09-21 21:46:34 +0200

    Add files via upload

---
+++ ytdl simplifier.bat
@@ -0,0 +1,30 @@
+@echo off
+
+:decide
+title ytdl simplifier
+cls
+echo. 
+SET /P choice= "Video(1) oder Sound(2): "
+IF "%choice%"=="1" GOTO :video
+IF "%choice%"=="2" GOTO :sound
+GOTO :decide
+
+:video
+title ytdl simplifier - Video downloader
+cls
+echo.
+SET /P videourl= "Video | YouTube Link: "
+youtube-dl.exe --format "bestvideo+bestaudio[ext=m4a]/bestvideo+bestaudio/best" --merge-output-format mp4 %videourl%
+echo Done!
+timeout /t 2 /nobreak > nul
+GOTO :video
+
+:sound
+title ytdl simplifier - mp3 downloader
+cls
+echo.
+SET /P soundurl= "Sound | YouTube Link: "
+youtube-dl.exe -f "bestaudio/best" -ciw -o "%%(title)s.%%(ext)s" -v --extract-audio --audio-quality 0 --audio-format mp3 %soundurl%
+echo Done!
+timeout /t 2 /nobreak > nul
+GOTO :sound
