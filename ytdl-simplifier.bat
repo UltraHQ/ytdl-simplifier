822210ab666ca0c40b5d6d2f47a7c8f5cd83aba7
UltraHQ <35925402+UltraHQ@users.noreply.github.com>
2022-01-18 13:35:10 +0100

    Add files via upload

--- ytdl-simplifier.bat
+++ ytdl-simplifier.bat
@@ -1,5 +1,16 @@
 @echo off
 
+:: Check if youtube-dl.exe exists
+if exist youtube-dl.exe (
+GOTO :decide
+) else (
+echo Error: youtube-dl.exe not found
+echo Please download and place youtube-dl.exe in the same directory as the ytdl-simplifier.bat
+pause
+exit
+)
+
+:: Decision if video or sound download is wanted
 :decide
 title ytdl simplifier
 cls
@@ -7,8 +18,19 @@ echo.
 SET /P choice= "Video(1) or Sound(2): "
 IF "%choice%"=="1" GOTO :video
 IF "%choice%"=="2" GOTO :sound
-GOTO :decide
+GOTO :decidefail
 
+:: If user types anything but 1 or 2 it will fail and give a message
+:decidefail
+title ytdl simplifier
+cls
+echo Error: Invalid choice
+SET /P choice= "Video(1) or Sound(2): "
+IF "%choice%"=="1" GOTO :video
+IF "%choice%"=="2" GOTO :sound
+GOTO :decidefail
+
+:: Passing arguments for video downloading in mp4 using best quality
 :video
 title ytdl simplifier - Video downloader
 cls
@@ -19,6 +41,7 @@ echo Done!
 timeout /t 2 /nobreak > nul
 GOTO :video
 
+:: Passing arguments for sound downloading in mp3 using best quality
 :sound
 title ytdl simplifier - mp3 downloader
 cls
